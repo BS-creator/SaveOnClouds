c513f67492c13bb7f8c236ff63d88bc389cd3666
Bestsolution <bestsolution2028@outlook.com>
2020-02-05 18:50:45 +0800

    fix step 3

--- SaveOnClouds.Web/Controllers/AccountsController.cs
+++ SaveOnClouds.Web/Controllers/AccountsController.cs
@@ -10,7 +10,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SaveOnClouds.Web.Models.Accounts;
-using SaveOnCyountsController(ILogger<AccountsController> logger, UserManager<IdentityUser> userManager,
+using SaveOnClouds.Web.Services;
+using SaveOnClouds.Web.Services.DataAccess;
+
+namespace SaveOnClouds.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly IEmailSender _emailSender;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly ILogger _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IDataAccess _dataAccess;
+
+        public AccountsController(ILogger<AccountsController> logger, UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager, IEmailSender emailSender, IWebHostEnvironment hostingEnvironment,
             IDataAccess dataAccess)
         {
