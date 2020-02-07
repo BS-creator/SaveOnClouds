9f763939ea3cc6de71e8177b834545c1fcfd7877
Bestsolution <bestsolution2028@outlook.com>
2020-02-07 15:22:52 +0800

    fix

--- SaveOnClouds.Web/Controllers/AccountsController.cs
+++ SaveOnClouds.Web/Controllers/AccountsController.cs
@@ -10,21 +10,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SaveOnClouds.Web.Models.Accounts;
-// using SaveOnClouds.Web.Services;
-// using SaveOnClouds.Web.Services.DataAccess;
+using SaveOnClouds.Web.Services;
+using SaveOnClouds.Web.Services.DataAccess;
 
-// namespace SaveOnClouds.Web.Controllers
-// {
-//     public class AccountsController : Controller
-//     {
-//         private readonly IEmailSender _emailSender;
-//         private readonly IWebHostEnvironment _hostingEnvironment;
-//         private readonly ILogger _logger;
-//         private readonly SignInManager<IdentityUser> _signInManager;
-//         private readonly UserManager<IdentityUser> _userManager;
-//         private readonly IDataAccess _dataAccess;
+namespace SaveOnClouds.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly IEmailSender _emailSender;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly ILogger _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IDataAccess _dataAccess;
 
-//         public AccountsController(ILogger<AccountsController> logger, UserManager<IdentityUser> userManager,
+        public AccountsController(ILogger<AccountsController> logger, UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager, IEmailSender emailSender, IWebHostEnvironment hostingEnvironment,
             IDataAccess dataAccess)
         {
@@ -43,7 +43,7 @@ using SaveOnClouds.Web.Models.Accounts;
         public async Task<IActionResult> SignOut()
         {
             await _signInManager.SignOutAsync();
-            // return RedirectToAction("SignIn");
+            return RedirectToAction("SignIn");
         }
 
         #endregion
